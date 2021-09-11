c4fee72f7ddf24d096cc8e8d1e47d974ca8f6a21
Akash Wilson M <akashwilsonm@gmail.com>
2021-09-11 19:58:28 +0530

    modified port accessing

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import random
 import os
 
 TOKEN = os.environ['API_KEY']
-PORT = int(os.environ.get('PORT', '8443'))
+PORT = environ['PORT']
 URL = os.environ['URL']
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
