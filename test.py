3668ded1b2414aad30b7d78bdc3ef19cf6c326dd
Akash Wilson M <akashwilsonm@gmail.com>
2021-10-09 22:49:53 +0530

    initial commit for version 2

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import random
 import os
+import psycopg2
 
 TOKEN = os.environ['API_KEY']
 PORT = os.environ.get('PORT')
 URL = os.environ['URL']
+DATABASE = "postgres://tfljkvwj:IbnOgi6XgAbgYF6LlO1Hdd9JzJYlwzvt@john.db.elephantsql.com/tfljkvwj"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -50,13 +52,38 @@ def echo(update, context):
 
     context.bot.sendMessage(chat_id = update.effective_chat.id, text = out)
 
+def theri(update, context):
+
+    reply = ''
+
+    if(len(context.args) == 0):
+        reply = "nthelum konachatt po kunne"
+
+    else:
+        theristr = ''
+
+        for i in context.args:
+            theristr += i
+
+        theris = theristr.split(',')
+
+        for theri in theris:
+            conn = psycopg2.connect(DATABASE)
+            cursor = conn.cursor()
+            cursor.execute(f"insert into (theri) values ({theri})")
+
+        reply = 'theri poottikkettind'
+
+    context.bot.sendMessage(chat_id = update.effective_chat.id, text = reply)
 
 startHandler = CommandHandler('start', start)
 echoHandler = MessageHandler(Filters.text & (~Filters.command), echo)
 pooreHandler = CommandHandler('poore', poore)
+theriHandler = CommandHandler('theri', theri)
 dispatcher.add_handler(startHandler)
 dispatcher.add_handler(echoHandler)
 dispatcher.add_handler(pooreHandler)
+dispatcher.add_handler(theriHandler)
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN,
