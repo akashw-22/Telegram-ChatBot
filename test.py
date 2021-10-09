5e92cbde4f40f9b07e61a668ac1b6a64cf8bab7f
Akash Wilson M <akashwilsonm@gmail.com>
2021-10-10 01:11:54 +0530

    removed bug

--- test.py
+++ test.py
@@ -70,8 +70,9 @@ def theri(update, context):
         for theri in theris:
             conn = psycopg2.connect(DATABASE)
             cursor = conn.cursor()
-            cursor.execute(f"insert into (theri) values ('{theri})''")
-
+            cursor.execute(f"insert into (theri) values ('{theri}')")
+            reply += cursor.fetchall()
+        
         reply = 'theri poottikkettind'
 
     print(reply)
